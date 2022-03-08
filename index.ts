dd04fff30864035f4815adfe4565fd9b110b2064
Emanuele Capparelli <k@emkpp.com>
2022-03-08 23:50:49 +0300

    code review changes

--- index.ts
+++ index.ts
@@ -15,13 +15,37 @@ type IntercomPlugin = Plugin<{
 
 type IntercomMeta = PluginMeta<IntercomPlugin>
 
-async function setupPlugin({ config, global }: IntercomMeta) {
-    global.intercomUrl =
-        config.useEuropeanDataStorage === 'True' ? 'https://api.eu.intercom.com' : 'https://api.intercom.io'
+type JobRequest = {
+    email: string,
+    event: string,
+    userId: string,
+    timestamp: number,
 }
 
-async function onEvent(event: PluginEvent, { config, global }: IntercomMeta) {
-    if (!isEventValid(config.triggeringEvents, event.event)) {
+export const jobs = {
+    sendToIntercom: async (request: JobRequest, { global, config }: IntercomMeta): Promise<void> => {
+        const isContactInIntercom = await searchForContactInIntercom(global.intercomUrl, config.intercomApiKey, request.email)
+        if (!isContactInIntercom) {
+            return
+        }
+        await sendEventToIntercom(
+            global.intercomUrl,
+            config.intercomApiKey,
+            request.email,
+            request.event,
+            request.userId,
+            request.timestamp
+        )
+    }
+}
+
+export async function setupPlugin({ config, global }: IntercomMeta): Promise<void> {
+    global.intercomUrl =
+        config.useEuropeanDataStorage === 'Yes' ? 'https://api.eu.intercom.com' : 'https://api.intercom.io'
+}
+
+export async function onEvent(event: PluginEvent, { config, jobs }: IntercomMeta): Promise<void> {
+    if (!isTriggeringEvent(config.triggeringEvents, event.event)) {
         return
     }
     const email = getEmailFromEvent(event)
@@ -31,25 +55,18 @@ async function onEvent(event: PluginEvent, { config, global }: IntercomMeta) {
     if (!isEmailDomainValid(config.ignoredEmailDomains, email)) {
         return
     }
-    const isContactInIntercom = await searchContactInIntercom(global.intercomUrl, config.intercomApiKey, email)
-    if (!isContactInIntercom) {
-        return
-    }
 
-    const date = event['sent_at'] ? new Date(event['sent_at']) : new Date()
-    const timestamp = Math.floor(date.getTime() / 1000)
+    const timestamp = getTimestamp(event)
 
-    await sendEventToIntercom(
-        global.intercomUrl,
-        config.intercomApiKey,
+    jobs.sendToIntercom({
         email,
-        event.event,
-        event['distinct_id'],
-        timestamp
-    )
+        event: event.event,
+        userId: event['distinct_id'],
+        timestamp,
+    }).runNow()
 }
 
-async function searchContactInIntercom(url: string, apiKey: string, email: string) {
+async function searchForContactInIntercom(url: string, apiKey: string, email: string) {
     const searchContactResponse = await fetchWithRetry(
         `${url}/contacts/search`,
         {
@@ -72,7 +89,7 @@ async function searchContactInIntercom(url: string, apiKey: string, email: strin
 
     if (!statusOk(searchContactResponse) || searchContactResponseJson.errors) {
         const errorMessage = searchContactResponseJson.errors ? searchContactResponseJson.errors[0].message : ''
-        console.log(
+        console.error(
             `Unable to search contact ${email} in Intercom. Status Code: ${searchContactResponseJson.status}. Error message: ${errorMessage}`
         )
         return false
@@ -115,7 +132,7 @@ async function sendEventToIntercom(
             const sendEventResponseJson = sendEventResponse.json() as Record<string, any>
             errorMessage = sendEventResponseJson.errors ? sendEventResponseJson.errors[0].message : ''
         } catch {}
-        console.log(
+        console.error(
             `Unable to send event ${event} for ${email} to Intercom. Status Code: ${sendEventResponse.status}. Error message: ${errorMessage}`
         )
     } else {
@@ -163,7 +180,19 @@ function isEmailDomainValid(ignoredEmailDomains: string, email: string): boolean
     return emailDomainsToIgnore.indexOf(email.split('@')[1]) < 0
 }
 
-function isEventValid(triggeringEvents: string, event: string): boolean {
+function isTriggeringEvent(triggeringEvents: string, event: string): boolean {
     const validEvents = (triggeringEvents || '').split(',').map((e) => e.trim())
     return validEvents.indexOf(event) >= 0
+}
+
+function getTimestamp(event: PluginEvent): number {
+    try {
+        if (event['timestamp']) {
+            return Number(event['timestamp']);
+        }
+    } catch {
+        console.error('Event timestamp cannot be parsed as a number')
+    }
+    const date = event['sent_at'] ? new Date(event['sent_at']) : new Date()
+    return Math.floor(date.getTime() / 1000)
 }
